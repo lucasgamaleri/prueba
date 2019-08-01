6f23f1dab1732c7b7e49a6957b3a07d6961a1d6e
Lucas Axel Gamaleri <l.a.g@outlook.com.ar>
2019-08-01 17:02:49 -0300

    Agrega filtro de lotes con demora
    Se cuentan los lotes por cada cambio de ancho solo cuando el cambio de ancho generó demora.
    
    la salida en consola de comandos muestra
    [ubicacion en celda, ancho, cant lote]
    
    falta agregar que el lote sea menos de 10 bobinas y anexar a la salida informacion extraída de la tabla de demoras

--- prueba.py
+++ prueba.py
@@ -1,5 +1,6 @@
 from pandas import read_excel
 import xlrd
+#IMPORTACION DE BASE DE DATOS
 date = "31-07-2019"
 demoras = read_excel(date +' - Demoras.xls')
 produccion = read_excel(date +'.xlsx')
@@ -17,17 +18,25 @@ espesor = produccion[columns[4]]
 materialrefilado = produccion[columns[5]]
 #DEMORAS
 columns = list(demoras)
-bobinademora = demoras[columns[0]]
-horainicio = demoras[columns[1]]
-horafin = demoras[columns[2]]
+bobinademora = []#demoras[columns[0]]
+inicio = []#demoras[columns[1]]
+fin = []#demoras[columns[2]]
 duracion = demoras[columns[4]]
 subconcepto = demoras[columns[6]]
+
+for i in range(0, len(demoras)):
+        bobinademora.append(demoras[columns[0]][i])
+        inicio.append(demoras[columns[1]][i])
+        fin.append(demoras[columns[2]][i])
+
+
+
 
 
 # Lista de refilados
 refilado = []
 
-for i in range(1, len(produccion)):
+for i in range(0, len(produccion)):
     if materialrefilado[i] == 0:
         refilado.append(False)
     else:
@@ -36,16 +45,29 @@ for i in range(1, len(produccion)):
 
 # Lista de cambios de ancho
 cambiodeancho = []
-for i in range(1,len(produccion)-1):
+for i in range(0,len(produccion)-1):
     if ancho[i+1] == ancho[i]:
         cambiodeancho.append(False)
     else:
         cambiodeancho.append(True)
 
+# Lista de Lotes
+lotes = []
+count = 1
+
+for i in range(1, len(produccion)):
+        if ancho[i-1] == ancho[i]:
+                count = count+1
+                
+        else:
+                if bobina[i] in bobinademora:
+                        lotes.append([i, ancho[i], count])
+                        count = 1
+                count = count + 1
+
+print(*lotes, sep="\n")    
 
 
-print(ancho)
-print(cambiodeancho)
 
 #print(demoras.head(3)) IMPRIME ENCABEZADO CON PRIMERAS 3 BOBINAS DE LISTA DE DEMORAS
 #print(produccion.head(3)) IMPRIME ENCABEZADO CON PRIMERAS 3 BOBINAS
