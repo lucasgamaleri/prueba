bdc019046c7ae4778193fca4f542a8be6a32a97b
Lucas Axel Gamaleri <l.a.g@outlook.com.ar>
2019-08-02 12:02:09 -0300

    Basado en DataFrame
    Se prueba trabajar en DataFrame en lugar de listas para mantener la organizacion de la tabla
    
    crea una lista con los lotes donde hubo demoras
    
    Falta estructurar una tabla con los lotes y la hora de la demora correspondiente a la ultima bobina y la duracion de la demora correspondiente a la ultima bobina

--- prueba.py
+++ prueba.py
@@ -1,75 +1,48 @@
+import pandas as pd
 from pandas import read_excel
 import xlrd
+
+#Definiciones y funciones
+def print_full(x):
+        pd.set_option('display.max_rows', len(x))
+        print(x)
+        pd.reset_option('display.max_rows')
+
 #IMPORTACION DE BASE DE DATOS
 date = "31-07-2019"
 demoras = read_excel(date +' - Demoras.xls')
+demoras.set_index('Bobina')
 produccion = read_excel(date +'.xlsx')
-
-
-
-
-#PRODUCCION
-columns = list(produccion) #Lista las columnas de la tabla de produccion
-fechaprod = produccion[columns[0]]
-bobina = produccion[columns[1]]
-grupocal = produccion[columns[2]]
-ancho = produccion[columns[3]]
-espesor = produccion[columns[4]]
-materialrefilado = produccion[columns[5]]
-#DEMORAS
-columns = list(demoras)
-bobinademora = []#demoras[columns[0]]
-inicio = []#demoras[columns[1]]
-fin = []#demoras[columns[2]]
-duracion = demoras[columns[4]]
-subconcepto = demoras[columns[6]]
-
-for i in range(0, len(demoras)):
-        bobinademora.append(demoras[columns[0]][i])
-        inicio.append(demoras[columns[1]][i])
-        fin.append(demoras[columns[2]][i])
-
+produccion.set_index('Bobina')
 
 
 
 
 # Lista de refilados
-refilado = []
-
-for i in range(0, len(produccion)):
-    if materialrefilado[i] == 0:
-        refilado.append(False)
-    else:
-            refilado.append(True)
+produccion = produccion.assign(REFILADO=lambda x: produccion['Material Refilado']==0)
 
 
 # Lista de cambios de ancho
-cambiodeancho = []
+cambiodeancho = [False]
 for i in range(0,len(produccion)-1):
-    if ancho[i+1] == ancho[i]:
+    if produccion.at[i+1,'Ancho'] == produccion.at[i,'Ancho']:
         cambiodeancho.append(False)
     else:
         cambiodeancho.append(True)
 
 # Lista de Lotes
 lotes = []
+index = []
 count = 1
 
-for i in range(1, len(produccion)):
-        if ancho[i-1] == ancho[i]:
-                count = count+1
-                
+for i in range(1, len(produccion)):        
+        if produccion.at[i-1,'Ancho'] == produccion.at[i,'Ancho']:
+                count = count+1                
         else:
-                if bobina[i] in bobinademora:
-                        lotes.append([i, ancho[i], count])
+                if list(produccion.index)[i] in list(demoras.index):
+                        lotes.append(count)        
                         count = 1
-                count = count + 1
-
-print(*lotes, sep="\n")    
-
-
-
-#print(demoras.head(3)) IMPRIME ENCABEZADO CON PRIMERAS 3 BOBINAS DE LISTA DE DEMORAS
-#print(produccion.head(3)) IMPRIME ENCABEZADO CON PRIMERAS 3 BOBINAS
-#print(produccion['Bobina'])  IMPRIME LISTA DE BOBINAS
-#print(fechaprod) IMPRIME LISTA DE FECHAS
+                else:
+                        count = count+1
+                
+print(lotes)
