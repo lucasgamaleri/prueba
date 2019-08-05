50818bef23f7a83345a2544c5b3441f5187c79c5
Lucas Axel Gamaleri <l.a.g@outlook.com.ar>
2019-08-05 13:15:22 -0300

    Beta
    Se finaliza representacion de tabla con resultados
    
    Se continua testing de validez de datos
    
    Se agrega interfaz de consola para escribir nombre de archivo de demoras y de produccion

--- prueba.py
+++ prueba.py
@@ -9,14 +9,19 @@ def print_full(x):
         pd.reset_option('display.max_rows')
 
 #IMPORTACION DE BASE DE DATOS
-#demoras = input('Archivo de datos de demoras >> ')
-#produccion = input('Archivo de datos de produccion >> ')
-date = "31-07-2019"
-demoras = read_excel(date +' - Demoras.xlsx')
-demoras.set_index('Bobina')
-produccion = read_excel(date +'.xlsx')
-produccion.set_index('Bobina')
+demoras = input('Archivo de datos de demoras >> ')
+produccion = input('Archivo de datos de produccion >> ')
+#date = "31-07-2019"
 
+#demoras = read_excel(date +' - Demoras.xlsx')
+#demoras.set_index('Bobina')
+#produccion = read_excel(date +'.xlsx')
+#produccion.set_index('Bobina')
+
+demoras = read_excel(demoras)
+demoras.set_index('Bobina')
+produccion = read_excel(produccion)
+produccion.set_index('Bobina')
 
 
 
@@ -31,22 +36,31 @@ for i in range(0,len(produccion)-1):
         cambiodeancho.append(False)
     else:
         cambiodeancho.append(True)
-print(cambiodeancho)
+
 # Lista de Lotes
 lotes = []
 index = []
 count = 1
-
+fecha = []
+refil = []
+producto = []
+dimension = []
 for i in range(1, len(produccion)):        
         if produccion.at[i-1,'Ancho'] == produccion.at[i,'Ancho']:
                 count = count+1                
         else:
-                if list(produccion.index)[i] in list(demoras.index):
-                        lotes.append(count)        
+                if list(produccion.index)[i] in list(demoras.index): # and demoras.at[list(produccion.index)[i],'Duracion']>=3: #demoras.Duracion[list(produccion.index)[i]]>1: #
+                        lotes.append(count)
+                        fecha.append(produccion.at[list(produccion.index)[i],'Fecha Produccion'])
+                        refil.append(produccion.at[list(produccion.index)[i],'REFILADO'])
+                        producto.append(produccion.at[list(produccion.index)[i],'Grupo Calidad'])
+                        dimension.append(str(produccion.at[list(produccion.index)[i],'Espesor'])+'x'+str(produccion.at[list(produccion.index)[i],'Ancho']))
                         count = 1
                 else:
                         count = count+1
-        print(count)
 print(produccion)
-print(demoras)                
-print(lotes)
+print(demoras.Duracion)
+
+result = {'Fecha Produccion': fecha, 'Dimension': dimension,'Cantidad de bobinas en el lote': lotes, 'Refilado': refil, 'Producto': producto}
+result = pd.DataFrame(data=result)
+print(result)
